fcfdc4f99c1e62916d57d687e2680da2003cf5f6
Mark Ormesher <me@markormesher.co.uk>
2017-12-30 12:47:38 +0000

    Real datatable querying for users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,9 @@
 import {Request, Response, Router} from 'express';
+import {Op} from 'sequelize'
+import {Promise} from 'bluebird';
 
 import AuthHelper = require('../helpers/auth');
+import {User} from '../models/User';
 
 const router = Router();
 
@@ -14,16 +17,39 @@ router.get('/', AuthHelper.restrict(['users.view']), (req: Request, res: Respons
 });
 
 router.get('/data', AuthHelper.restrict(['users.view']), (req: Request, res: Response) => {
+	const columns = ['username', 'displayName'];
+
 	const start = parseInt(req.query['start']);
 	const count = parseInt(req.query['length']);
-	const order = req.query['order'][0]['dir'];
+	const order = req.query['order'];
 	const search = req.query['search']['value'];
 
-	res.json({
-		recordsTotal: 0,
-		recordsFiltered: 0,
-		data: []
-	})
+	const queryOrder = order.map((o: { column: string, dir: string }) => [columns[parseInt(o.column)], o.dir]);
+
+	Promise.all([
+		User.findAll({
+			where: {
+				[Op.or]: {
+					username: {[Op.iLike]: `%${search}%`},
+					displayName: {[Op.iLike]: `%${search}%`}
+				}
+			},
+			order: queryOrder,
+			offset: start,
+			limit: count
+		}),
+		User.count()
+	]).then(results => {
+		const [users, countAllUsers] = results;
+		res.json({
+			recordsTotal: countAllUsers,
+			recordsFiltered: users.length,
+			data: users
+		})
+	}).error(err => {
+		console.log(err);
+		res.status(500).end();
+	});
 });
 
 export = router;
